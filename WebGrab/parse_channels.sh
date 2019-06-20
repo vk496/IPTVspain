351871cdb719d49d9738a0e1be54ac5b71c84564
Valentín Kivachuk <vk18496@gmail.com>
2019-06-20 14:25:14 +0200

    Radio support + Europa FM

--- WebGrab/parse_channels.sh
+++ WebGrab/parse_channels.sh
@@ -5,7 +5,7 @@ EPG_url="https://raw.githubusercontent.com/vk496/IPTVspain/master/guide.xml"
 echo "#EXTM3U @vk496 https://github.com/vk496/IPTVspain" > spain.m3u8
 echo "#EXTM3U url-tvg=\"${EPG_url}\"" >> spain.m3u8
 
-cat README.md | sed -n '/canales hay/,/Agradecimientos/{/canales hay/b;/Agradecimientos/b;p}' | tail -n +3 | \
+cat README.md | sed -n '/canales hay/,/Agradecimientos/{/canales hay/b;/Agradecimientos/b;p}' | grep "^|" |tail -n +3 | \
 while read line; do
 	img=$(echo $line | cut -d\| -f2 | cut -d\" -f2)
 	name=$(echo $line | cut -d\| -f3)
@@ -22,6 +22,12 @@ while read line; do
 		fi
 	fi
 
-	echo "#EXTINF:-1 group-title=\"$categ\" tvg-id=\"$epgID\" tvg-logo=\"$img\" url-epg=\"$EPG_url\",$name" >> spain.m3u8
+	if [[ ${categ,,} == "radio" ]]; then
+		radio=" radio=\"true\""
+	else
+		radio=""
+	fi
+
+	echo "#EXTINF:-1 group-title=\"$categ\" tvg-id=\"$epgID\" tvg-logo=\"$img\" url-epg=\"$EPG_url\"${radio},$name" >> spain.m3u8
 	echo "$m3u8" >> spain.m3u8
 done
